cec847d4384f0fdc50b2f06d73c5d245e8b8a990
jmorrisart <morris.jess26@gmail.com>
2021-05-28 22:33:34 -0600

    revised contact page so desktop view is not crooked

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AppService } from '../../app.service';
 import { FormControl } from '@angular/forms';
 
+import { init } from 'emailjs-com';
+
 @Component({
   selector: 'contact',
   templateUrl: './contact.component.html',
@@ -9,10 +11,23 @@ import { FormControl } from '@angular/forms';
 })
 export class ContactComponent implements OnInit {
 
+  public name: any;
+  public email: any;
+  public message: any;
+
   constructor( private appService: AppService ) { }
 
   ngOnInit(): void {
     this.appService.setMobileMenuStyling();
+  }
+
+  processForm() {
+    const allInfo = 'My name is ' + this.name + '. My email is ' + this.email + '. My message is ' + this.message;
+    this.initEmailJS();
+  }
+
+  initEmailJS() {
+    init("service_kvgjoyb");
   }
 
 }
